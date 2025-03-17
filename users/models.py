4d586695f1589315ec4c51bd7a694bfb21596bc2
NitinV84 <nitinvdev601@gmail.com>
2025-03-17 14:51:27 +0530

    Added UserProfile model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.gis.db import models as geomodels
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    home_address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    location = geomodels.PointField()
+    
+    def __str__(self):
+        return self.user.username
