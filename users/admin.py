9b9abc84d78306943c69f739f070fb056dd6c550
NitinV84 <nitinvdev601@gmail.com>
2025-03-18 14:14:18 +0530

    Implement user registration by admin with email confirmation

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,68 @@
 from django.contrib import admin
-from .models import UserProfile
+from django.contrib.auth.admin import UserAdmin
+from django.utils.crypto import get_random_string
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.conf import settings
+from .models import CustomUser, UserProfile
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from .forms import CustomUserCreationForm
 
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "phone_number", "location"]
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "first_name", "last_name", "password"),
+            },
+        ),
+    )
+
+    def save_model(self, request, obj, form, change):
+        """Override save to send invitation email when a new user is created."""
+        is_new_user = not obj.pk  # Check if user is newly created
+        if is_new_user and obj.password:
+            obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
+
+        if is_new_user:
+
+            # Create user profile
+            UserProfile.objects.get_or_create(user=obj)
+            # Generate a valid password reset token
+            uid = urlsafe_base64_encode(force_bytes(obj.pk))
+            token = default_token_generator.make_token(obj)
+
+            # Correct password reset link
+            reset_link = request.build_absolute_uri(
+                reverse(
+                    "password_reset_confirm", kwargs={"uidb64": uid, "token": token}
+                )
+            )
+
+            subject = "You're invited to our platform"
+            message = f"Hello {obj.get_full_name()},\n\nYou've been registered by an admin.\nPlease click the link below to set your password:\n{reset_link}\n\nThank you!"
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [obj.email])
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserProfile)
--- users/models.py
+++ users/models.py
@@ -1,13 +1,37 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.gis.db import models as geomodels
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    home_address = models.TextField()
-    phone_number = models.CharField(max_length=15)
-    location = geomodels.PointField()
+class CustomUser(AbstractUser):
+    """
+    Custom user model extending Django's AbstractUser.
     
+    Attributes:
+        email (EmailField): The unique email address of the user.
+    """
+    email = models.EmailField(unique=True)
+
     def __str__(self):
+        """Returns the username of the user."""
+        return self.username
+
+
+class UserProfile(models.Model):
+    """
+    User profile model extending the CustomUser model.
+    
+    Attributes:
+        user (OneToOneField): A one-to-one relationship with CustomUser.
+        home_address (TextField): The home address of the user.
+        phone_number (CharField): The phone number of the user.
+        location (PointField): The geographic location of the user.
+    """
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    home_address = models.TextField(null=True)
+    phone_number = models.CharField(max_length=15, null=True)
+    location = geomodels.PointField(null=True)
+
+    def __str__(self):
+        """Returns the username of the associated user."""
         return self.user.username
